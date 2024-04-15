0bbdfe812efb50bb63644d36792afec5216d6f69
Tor4ok <tima.sozin@gmail.com>
2024-04-15 13:38:42 +0300

    Added basic stuff.

---
+++ MMATW-game/Assets/MMATW/Scripts/CoolDown.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace MMATW.Scripts
+{
+    [System.Serializable]
+    public class CoolDown : MonoBehaviour
+    {
+        [SerializeField] private float cooldownTime;
+        private float _nextCDtime;
+
+        public bool IsInCooldown => Time.time < _nextCDtime;
+        public void StartCooldown() => _nextCDtime = Time.time + cooldownTime;
+    }
+}
