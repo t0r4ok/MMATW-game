3a40d846fdbd4b6e678721a031ab63a6dff5d3cc
kArrKaDe <maxon130309@gmail.com>
2024-04-19 19:01:47 +0500

    UI

---
+++ MMATW-game/Assets/MMATW/Scripts/UI/MenuManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject PauseMenu;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            gameObject.SetActive(false);
+            PauseMenu.SetActive(true);
+        }
+    }
+}
